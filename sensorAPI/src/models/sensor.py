328a0fdb3af791fc5e6c658f45932ac0085e2d48
Carlos Camacho <carloscamacho0602@gmail.com>
2025-05-29 00:56:36 -0500

    Se añaden modelo de sensor inicial y archivos de modulos.

---
+++ sensorAPI/src/models/sensor.py
@@ -0,0 +1,13 @@
+from dataclasses import dataclass
+from datetime import datetime
+from typing import Optional
+
+
+@dataclass
+class Sensor:
+    id: Optional[int] = None
+    nombre_sensor: str = ""
+    tipo_sensor: str = ""
+    ubicacion_sensor: str = ""
+    estado_sensor: str = "activo"
+    fecha_creacion: Optional[datetime] = None
